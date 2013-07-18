0e91a3f9875ea208bded4be36366d7d435107bb4
datawolf <wanglong@cse.buaa.edu.cn>
2013-07-18 15:32:11 +0800

    use setjmp in 023-alarm-1.c

---
+++ 023-alarm-1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <setjmp.h>
+static  jmp_buf     env_alrm;
+
+static void sig_alarm(int signo)
+{
+    longjmp(env_alrm, 1);
+}
+
+int main(int argc, char **argv)
+{
+    int     n;
+    char    line[300];
+
+    if (signal(SIGALRM, sig_alarm) == SIG_ERR)
+    {
+        perror("signal(SIGALRM) error");
+        exit(0);
+    }
+    
+    if (setjmp(env_alrm) != 0)
+    {
+        printf("read timeout\n");
+        exit(0);
+    }
+    alarm(10);
+    if ((n = read(STDIN_FILENO, line, 300)) < 0)
+    {
+        perror("read error");
+        exit(0);
+    }
+
+    alarm(0);
+
+    write(STDOUT_FILENO, line, n);
+
+    exit(0);
+}
+/*
+ * 在linux下，系统调用是重新启动的，当从SIGALRM信号
+ * 处理程序返回时，read并不被中断，此时，设置时间
+ * 限制不起作用。i
+ *
+ * 详细请参考：APUE page 256页
+ *
+ * */
+
+
