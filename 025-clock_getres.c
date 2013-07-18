2219b3653566ada0ac241013423f7d59010cf616
datawolf <wanglong@cse.buaa.edu.cn>
2013-07-18 16:37:28 +0800

    add clock_getres example

--- 025-clock_getres.c
+++ 025-clock_getres.c
@@ -1,12 +1,11 @@
-/**
- * 编译时使用参数  -lrt
- * 
- *
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * 获取时间来源的分辨率
+ * 编译时使用参数  -lrt
+ */
+
 
 int main(int argc, char **argv)
 {
@@ -25,17 +24,27 @@ int main(int argc, char **argv)
 
     for (i = 0; clocks[i] != (clockid_t) -1; i++)
     {
-        struct      timespec        ts;
+        struct      timespec        res;
         int         ret;
 
-        ret = clock_gettime(clocks[i], &ts);
+        ret = clock_getres(clocks[i], &res);
 
         if (ret != 0)
             perror("clock_gettime");
         else
             printf("clock = %d sec = %ld nsec = %ld\n",
-                    clocks[i], ts.tv_sec, ts.tv_nsec);
+                    clocks[i], res.tv_sec, res.tv_nsec);
     }
 
     return EXIT_SUCCESS;
 }
+
+/**
+clock = 0 sec = 0 nsec = 1
+clock = 1 sec = 0 nsec = 1
+clock = 2 sec = 0 nsec = 1
+clock = 3 sec = 0 nsec = 1
+
+从结果上来看，这四个时间来源都绑定到了分辨率较高的定时器上。
+TSC（Timestamp counter）时间戳计数器，它能够提供那秒级的分辨率。
+*/
