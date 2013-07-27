815f3a483a93992af551994fdae51d9be08f1fdd
datawolf <wanglong@cse.buaa.edu.cn>
2013-07-27 21:48:37 +0800

    add clock_getcpuclockid example

---
+++ 033-clock_getcpuclockid.c
@@ -0,0 +1,36 @@
+/**
+ *
+ * compile with -lrt
+ */
+
+#define     _XOPEN_SOURCE   600
+#include    <stdio.h>
+#include    <unistd.h>
+#include    <stdlib.h>
+#include    <time.h>
+
+int main(int argc, char **argv)
+{
+    clockid_t     clock_id;
+    struct      timespec    ts;
+
+    if (argc != 2){
+        fprintf(stderr, "%s <process ID>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (clock_getcpuclockid(atoi(argv[1]), &clock_id) != 0 ){
+        perror("clock_getcpuclockid");
+        exit(EXIT_FAILURE);
+    }
+
+    if (clock_gettime(clock_id, &ts) == -1){
+        perror("clock_gettime");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("CPU-time clock for PID %s is %ld.%09ld seconds\n",
+            argv[1], (long) ts.tv_sec, (long) ts.tv_nsec);
+
+    exit(EXIT_SUCCESS);
+}
